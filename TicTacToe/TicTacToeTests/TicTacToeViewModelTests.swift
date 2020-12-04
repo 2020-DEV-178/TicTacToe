a7c7bb4ffdd8b256a1dfa78fe29a8dd520ccccf2
2020-DEV-178 <>
2020-12-04 13:18:18 +0100

    Add basic logic unit test functions (no implementation)

--- TicTacToe/TicTacToeTests/TicTacToeViewModelTests.swift
+++ TicTacToe/TicTacToeTests/TicTacToeViewModelTests.swift
@@ -1,14 +1,13 @@
 //
-//  TicTacToeTests.swift
+//  TicTacToeViewModelTests.swift
 //  TicTacToeTests
 //
 //  Created by Jonas Freres on 04/12/2020.
 //
 
 import XCTest
-@testable import TicTacToe
 
-class TicTacToeTests: XCTestCase {
+class TicTacToeViewModelTests: XCTestCase {
 
     override func setUpWithError() throws {
         // Put setup code here. This method is called before the invocation of each test method in the class.
@@ -17,17 +16,41 @@ class TicTacToeTests: XCTestCase {
     override func tearDownWithError() throws {
         // Put teardown code here. This method is called after the invocation of each test method in the class.
     }
+    
+    
 
-    func testExample() throws {
-        // This is an example of a functional test case.
-        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    func testEmptyGame() throws {
     }
 
+    func testPlayerMove() throws {
+    }
+    
+    func testTowPkayerMoves() throws {
+        
+    }
+    
+    func testPlayerMoveToOccupiedCase() throws {
+        
+    }
+    
+    func testFullGrame() throws {
+        
+    }
+    
+    func testPlayer1Win() throws {
+        
+    }
+    
+    func testPlayer2Win() throws {
+        
+        
+    }
+    
+    
     func testPerformanceExample() throws {
         // This is an example of a performance test case.
         self.measure {
             // Put the code you want to measure the time of here.
         }
     }
-
 }
